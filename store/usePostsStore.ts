d106d4ba80ec77bcb33c87ee56a0dac282ea7b69
hibik17 <hbtatsu@gmail.com>
2022-10-20 00:50:51 +0900

    update axios to onMounted

--- store/usePostsStore.ts
+++ store/usePostsStore.ts
@@ -1,18 +1,15 @@
 import { defineStore } from "pinia";
-import { onServerPrefetch } from "vue";
 import axios from "axios";
 
 export const usePostsStore = defineStore("usePostsStore", () => {
   const data = "this is from pinia";
 
   // all posts
-  const posts = reactive[[]];
-  axios
-    .get("https://jsonplaceholder.typicode.com/comments?postId=1")
-    .then((res) => posts.push(...res.data));
-
+  const posts = reactive([]);
   onMounted(() => {
-    console.log(posts);
+    axios
+      .get("https://jsonplaceholder.typicode.com/comments?postId=1")
+      .then((res) => posts.push(...res.data));
   });
 
   return { data, posts };
