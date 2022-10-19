a4f94e0642716111e88cb2fd9275fdf63f49c430
hibik17 <hbtatsu@gmail.com>
2022-10-19 23:51:39 +0900

    add pages

---
+++ pages/books.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+const msg = "this is books index vue";
+</script>
+
+<template>
+  <div>{{ msg }}</div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <div>this is index vue</div>
+    <NuxtLink to="/books">books</NuxtLink>
+  </div>
+</template>
