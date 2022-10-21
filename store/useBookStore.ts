b5bfacd1eed6a762a07e19194736d0ae83c67725
hibik17 <hbtatsu@gmail.com>
2022-10-21 17:57:02 +0900

    define useBookStore

--- store/useBookStore.ts
+++ store/useBookStore.ts
@@ -1,7 +1,27 @@
 import { defineStore } from "pinia";
 
-export const useBookStore = defineStore("useBookStore", () => {
-  const books = reactive([]);
+export type Book = {
+  title: string;
+  body: string;
+};
 
-  const addBook;
+export const useBookStore = defineStore("useBookStore", () => {
+  const newBook = reactive<Book>({
+    title: "",
+    body: "",
+  });
+
+  const books: Array<Book> = reactive<Array<Book>>([]);
+
+  const addBook = () => {
+    books.push();
+    console.log("新規投稿が完了");
+  };
+
+  const deleteBook = (index: number) => {
+    books.splice(index, 1);
+    console.log("削除が完了しました");
+  };
+
+  return { newBook, addBook, deleteBook };
 });
