c7cc09d020eff973666aa0d4d1d7273f7617ea98
hibik17 <hbtatsu@gmail.com>
2022-10-24 23:42:01 +0900

    revise header

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,15 @@
 <template>
-  <div>
-    <div class="text-red-400">this is index vue</div>
-    <NuxtLink to="/books">books</NuxtLink>
+  <div class="flex items-center justify-center w-screen h-screen">
+    <div>
+      <div class="text-3xl font-bold text-center text-gray-600">
+        welocome to bookers!
+      </div>
+      <div class="mt-10 font-serif text-xl text-center text-gray-600">
+        you can post your comment of the book that your read
+      </div>
+      <NuxtLink to="/books" class="flex items-center w-full">
+        Let's get started!
+      </NuxtLink>
+    </div>
   </div>
 </template>
