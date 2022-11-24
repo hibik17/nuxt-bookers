a142aa25a2d50a2c7397135707ed5cbf117a2a22
hibik17 <hbtatsu@gmail.com>
2022-11-24 18:37:09 +0900

    add axios action to edit page

--- pages/books/edit.vue
+++ pages/books/edit.vue
@@ -3,7 +3,12 @@
 </template>
 
 <script setup lang="ts">
+import axios from "axios";
 import { bookType } from "~~/components/types/bookType";
 
 const book = reactive<bookType>({ title: "", description: "" });
+
+axios.get("http://localhost:3000/api/v1/books/1").then((res) => {
+  const bookData = res.data;
+});
 </script>
